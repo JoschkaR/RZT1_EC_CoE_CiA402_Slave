abe3887d9cbc35e12a3f7e56314e2453b7db3663
JoschkaR <joschkaranderath@gmail.com>
2021-07-10 11:54:43 +0200

    Unnoetige Dtaen 2

--- project/sflash/RZ_T_sflash/HardwareDebug/src/common/loader_init.d
+++
@@ -1,2 +0,0 @@
-src/common/loader_init.d: \
- C:/Users/josch/OneDrive/Studium/Semester/6.\ Semester/Fachpraktikum/EtherCAT_CiA402/1_Workspace/workspace_selfmade_ssc/1_V2.x/EtherCAT_SSC_CiA402/src/common/loader_init.asm
--- project/sflash/RZ_T_sflash/HardwareDebug/src/sample/board_RenesasEva.d
+++
@@ -1,5 +0,0 @@
-src/sample/board_RenesasEva.d: \
- C:/Users/josch/OneDrive/Studium/Semester/6.\ Semester/Fachpraktikum/EtherCAT_CiA402/1_Workspace/workspace_selfmade_ssc/1_V2.x/EtherCAT_SSC_CiA402/src/sample/board_RenesasEva.c \
- c:\users\josch\onedrive\studium\semester\6.\ semester\fachpraktikum\ethercat_cia402\1_workspace\workspace_selfmade_ssc\1_v2.x\ethercat_ssc_cia402\inc\iodefine.h
-
-c:\users\josch\onedrive\studium\semester\6.\ semester\fachpraktikum\ethercat_cia402\1_workspace\workspace_selfmade_ssc\1_v2.x\ethercat_ssc_cia402\inc\iodefine.h:
